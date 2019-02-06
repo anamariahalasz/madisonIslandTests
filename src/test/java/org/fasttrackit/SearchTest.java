7f8326bfb1672455d796495902c4d60370a353ec
anamariahalasz <ana.mariahalasz@yahoo.com>
2019-02-06 19:53:31 +0200

    First test

---
+++ src/test/java/org/fasttrackit/SearchTest.java
@@ -0,0 +1,42 @@
+package org.fasttrackit;
+
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.List;
+
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+public class SearchTest {
+
+    @Test
+    public void searchByOneKeywordTest(){
+        System.setProperty("webdriver.chrome.driver", "src\\test\\resources\\drivers\\chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.get("http://fasttrackit.org/selenium-test");
+
+        String keyword = "vase";
+
+        driver.findElement(By.id("search")).sendKeys(keyword + Keys.ENTER);
+        //driver.findElement(By.linkText("WOMEN")).click();
+        //driver.findElement(By.partialLinkText("OMEN")).click();
+
+        List<WebElement> productNameContainers =
+                driver.findElements(By.cssSelector(".product-name > a"));
+
+        for (WebElement container : productNameContainers) {
+            String productName = container.getText();
+
+         //  System.out.println(productName);
+
+            assertThat("Some of the product names do not contain the searched keyword",
+                    productName, containsString(keyword.toUpperCase()));
+        }
+
+    }
+}
